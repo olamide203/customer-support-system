3923059766712b31f15c4c9e06eb794dfe82da36
Olamide Atitebi <olamideumarq@gmail.com>
2023-07-25 09:48:33 +0100

    bug fix

--- src/components/Notification/Dialog.tsx
+++ src/components/Notification/Dialog.tsx
@@ -21,7 +21,7 @@ const DialogDemo = ({ children }: DialogProps) => (
         <Trigger asChild>{children}</Trigger>
         <Portal>
             <Overlay className="bg-neutral-900/10 data-[state=open]:animate-overlayShow fixed inset-0 z-[300]" />
-            <Content className="data-[state=open]:animate-contentShow fixed top-[50%] left-[50%] w-[90vw] max-h-[80vh] overflow-y-auto max-w-[654px] translate-x-[-50%] translate-y-[-50%] rounded-[6px] bg-white py-[70px] px-[100px] shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] focus:outline-none z-[400]">
+            <Content className="data-[state=open]:animate-contentShow fixed top-[50%] left-[50%] w-[90vw] max-h-[80vh] overflow-y-auto max-w-[654px] translate-x-[-50%] translate-y-[-50%] rounded-[6px] bg-white p-6 md:p-10 lg:py-[70px] lg:px-[100px] shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] focus:outline-none z-[400]">
                 <Title className="text-neutral-900 m-0 text-[24px] font-semibold">
                     New Update
                 </Title>
@@ -86,7 +86,7 @@ const DialogDemo = ({ children }: DialogProps) => (
 
                 <Close asChild>
                     <button
-                        className="text-blue-400 hover:text-blue-500 border border-blue-400 absolute top-[75px] right-[100px] inline-flex h-[25px] w-[25px] appearance-none items-center justify-center"
+                        className="text-blue-400 hover:text-blue-500 border border-blue-400 absolute top-6 right-6 md:top-10 md:right-10 lg:top-[75px] lg:right-[100px] inline-flex h-[25px] w-[25px] appearance-none items-center justify-center"
                         aria-label="Close"
                         type="button"
                     >
