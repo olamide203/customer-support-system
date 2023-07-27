feeefce7b657cb90d7cebcf3b9c51ae947f622a3
benchike <benchike10@gmail.com>
2023-07-27 15:42:51 +0100

    updated admin dashbord

--- src/components/Text/InfoText.tsx
+++ src/components/Text/InfoText.tsx
@@ -1,4 +1,27 @@
-import React from 'react';
+import { keyframes } from '@emotion/react';
+import styled from '@emotion/styled';
+
+const blinkAnimation = keyframes`
+  0%, 100% {
+    color: red;
+  }
+  50% {
+    color: blue;
+  }
+`;
+
+const StyledInfoText = styled.div`
+    display: flex;
+    flex-direction: row;
+    gap: 1rem;
+    align-items: center;
+    animation: ${blinkAnimation} 2s infinite;
+
+    span {
+        font-family: 'Poppins';
+        font-size: 16px;
+    }
+`;
 
 interface Props {
     text: string;
@@ -6,11 +29,32 @@ interface Props {
 
 const InfoText = ({ text }: Props) => {
     return (
-        <div className="flex flex-row gap-1">
-            <img src="/icons/alert-circle.svg" alt="" />
-            <span className="font-poppins text-blue-400">{text}</span>
-        </div>
+        <StyledInfoText>
+            <img
+                src="/icons/alert-circle.svg"
+                alt=""
+                style={{ animation: ` ${blinkAnimation} 2s infinite` }}
+            />
+            <span>{text}</span>
+        </StyledInfoText>
     );
 };
 
 export default InfoText;
+
+// import React from 'react';
+
+// interface Props {
+//     text: string;
+// }
+
+// const InfoText = ({ text }: Props) => {
+//     return (
+//         <div className="flex flex-row gap-1 ">
+//             <img src="/icons/alert-circle.svg" alt="" />
+//             <span className="font-poppins text-blue-400 ">{text}</span>
+//         </div>
+//     );
+// };
+
+// export default InfoText;
