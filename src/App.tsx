ae60d8a793001b241386aab627868f16a5e2d8c1
Demitoye <demitoye6@gmail.com>
2023-07-28 12:51:24 +0100

    Added the toast package

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
-import React, { Suspense } from 'react';
+import React, { Suspense, useEffect } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { Routes } from './routing';
 import { Provider as ToastProvider } from '@radix-ui/react-toast';
 import { QueryClientProvider } from 'react-query';
 import { ReactQueryDevtools } from 'react-query/devtools';
 import { queryClient } from './hooks/react-query/queryClient';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const App: React.FC = () => {
     return (
@@ -12,6 +14,7 @@ const App: React.FC = () => {
             <BrowserRouter>
                 <ToastProvider>
                     <Routes />
+                    <ToastContainer />
                 </ToastProvider>
             </BrowserRouter>
             <ReactQueryDevtools />
